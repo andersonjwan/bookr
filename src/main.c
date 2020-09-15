060554158b46fa56d619c9897356dc95e7ebd605
andersonjwan <andersonjwan@outlook.com>
2020-09-14 21:27:21 -0700

    implemented base application window display

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include "main.h"
 
-int
-main(void)
+static void
+activate(GtkApplication *app,
+         gpointer        data)
 {
+  GtkBuilder *builder;
+  builder = gtk_builder_new_from_file("/home/andersonjwan/Projects/bookr/src/ui/main.ui");
 
-  return 0;
+  GtkWidget *window;
+  window = GTK_WIDGET(gtk_builder_get_object(GTK_BUILDER(builder),
+                                             "bookr-main-window"));
+
+  gtk_application_add_window(GTK_APPLICATION(app), GTK_WINDOW(window));
+
+  gtk_widget_show_all(GTK_WIDGET(window));
+}
+
+int
+main(int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new("com.github.andersonjwan.bookr", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
 }
