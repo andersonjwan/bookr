32a25724f6d737ce99ad4588bbb307c094a7dc76
andersonjwan <andersonjwan@outlook.com>
2020-09-21 17:58:54 -0700

    implemented popup of book add dialog

---
+++ src/book-add.c
@@ -0,0 +1,21 @@
+#include "book-add.h"
+
+static GtkBuilder *builder;
+static GtkWindow  *dialog;
+
+void
+show_book_add(void)
+{
+  builder = gtk_builder_new_from_file("/usr/share/bookr/gui/bookr-book-add.ui");
+  gtk_builder_connect_signals(GTK_BUILDER(builder), NULL);
+
+  dialog = get_dialog(builder, "bookr-book-add-dialog");
+  gtk_widget_show_all(GTK_WIDGET(dialog));
+}
+
+void
+hide_book_add(GtkButton *button,
+              gpointer   data)
+{
+  gtk_widget_destroy(GTK_WIDGET(dialog));
+}
---
+++ src/book-add.h
@@ -0,0 +1,9 @@
+#ifndef BOOK_ADD_H
+#define BOOK_ADD_H
+
+#include "builder.h"
+
+/* forward declarations */
+void show_book_add(void);
+void hide_book_add(GtkButton *, gpointer);
+#endif
