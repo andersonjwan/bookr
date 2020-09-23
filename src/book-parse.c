cd337124c8787e4ab9c2301f884c2fd26e482cb2
andersonjwan <andersonjwan@outlook.com>
2020-09-22 21:49:34 -0700

    renamed book-parse to book-parser

--- src/book-parse.c
+++
@@ -1,7 +0,0 @@
-#include "book-parse.h"
-
-void
-parse_book_file(void)
-{
-  return;
-}
--- src/book-parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOOK_PARSE_H
-#define BOOK_PARSE_H
-
-#include "main.h"
-
-/* forward declarations */
-void parse_book_file(void);
-
-#endif
