833f61a0336751a91c3ee8586d7eeca3d639e9ff
andersonjwan <andersonjwan@outlook.com>
2020-09-22 23:24:03 -0700

    implemented function to save list after app closed

---
+++ src/bookr-close.h
@@ -0,0 +1,10 @@
+#ifndef BOOKR_CLOSE_H
+#define BOOKR_CLOSE_H
+
+#include "main.h"
+
+/* forward declarations */
+void bookr_close(GtkApplication *, gpointer);
+static void bookr_write_book_list(void);
+
+#endif
