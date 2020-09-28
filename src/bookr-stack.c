7f7bacfdddd2ee849c33a78d427c9116b7cb3048
andersonjwan <andersonjwan@outlook.com>
2020-09-28 00:24:25 -0700

    implemented functions to update log stack header

--- src/bookr-stack.c
+++ src/bookr-stack.c
@@ -1,1 +1,33 @@
 #include "bookr-stack.h"
+
+static GtkBuilder *builder;
+
+void
+update_stacks(struct Book *book)
+{
+  if(!builder) {
+    builder = get_builder_builder();
+  }
+
+  update_log_stack(book);
+}
+
+static void
+update_log_stack(struct Book *book)
+{
+  update_log_stack_header(book);
+}
+
+static void
+update_log_stack_header(struct Book *book)
+{
+  GtkWidget *label;
+
+  /* update title label */
+  label = get_widget(builder,"bookr-main-stack-logs-header-label-title");
+  gtk_label_set_text(GTK_LABEL(label), book->title);
+
+  /* update author label */
+  label = get_widget(builder, "bookr-main-stack-logs-header-label-authors");
+  gtk_label_set_text(GTK_LABEL(label), book->author);
+}
--- src/bookr-stack.h
+++ src/bookr-stack.h
@@ -2,5 +2,15 @@
 #define BOOKR_STACK_H
 
 #include "main.h"
+#include "builder.h"
 
+#include "book.h"
+
+/* forward declarations */
+struct Book;
+
+void update_stacks(struct Book *);
+
+static void update_log_stack(struct Book *);
+static void update_log_stack_header(struct Book *);
 #endif
