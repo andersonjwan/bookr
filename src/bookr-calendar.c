dd2076da3e54cbe6b0bc077ddbfa26dacb7343bd
andersonjwan <andersonjwan@outlook.com>
2020-09-28 14:56:29 -0700

    formatted print call

--- src/bookr-calendar.c
+++ src/bookr-calendar.c
@@ -12,8 +12,7 @@ write_book_calendar(void)
     file = fopen(active->calendar, "r+");
 
     if(!file) {
-      g_printerr("Error %d: Unable to open file: %s\n",
-                 1, active->calendar);
+      g_printerr("Error %d: Unable to open file: %s\n", 1, active->calendar);
       exit(1);
     }
 
